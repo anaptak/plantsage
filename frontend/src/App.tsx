1c769c1093d8bb2248abe6f33a51b4fc3425ca1c
Ana Ptak <ptakanastasia@gmail.com>
2025-03-07 18:49:46 -0800

    adding temp background and text entry box

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Input } from "@/components/ui/input"
 import plantsageLogo from './assets/plantsage_logo.PNG'
 import './App.css'
 
@@ -10,7 +11,13 @@ function App() {
       <div className="center-container">
         <img src={plantsageLogo} className="logo" alt="PlantSage Logo" />
       </div>
+
       <h1>Plant Sage</h1>
+
+      <div className="flex justify-center my-4">
+        <Input placeholder="Enter the Name of a Plant" className="w-64" />
+      </div>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
