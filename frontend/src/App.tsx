91efcb349e87641c12ae90e668658cc3778bbbdb
Ana Ptak <ptakanastasia@gmail.com>
2025-05-24 23:26:39 -0700

    updating loading statement

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -180,7 +180,7 @@ function App() {
                   <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
                   <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4l3-3-3-3v4a8 8 0 000 16v-4l-3 3 3 3v-4a8 8 0 01-8-8z" />
                 </svg>
-                Loading...
+                Getting Plant Info...
               </>
             ) : (
               "Get Care Tips"
